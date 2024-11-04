d29c68b448e7683459011b6d1e4939ac4295f213
kntekopCOS101 <danmusausmain2@gmail.com>
2024-11-04 19:22:37 +0100

    (week 3 commit)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // addition
+    let sum = 5550 + 7310;
+    println!("the sum of 5550 and 7310 is {}",sum);
+
+    // subtraction
+    let difference = 5550 - 7310;
+    println!("the difference of 5550 and 7310 is {}",difference);
+
+
+    // multiplication
+    let product:f32 = 4.0 * 30.0;
+    println!("the product of 4 and 30 is {}",product);
+
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("the quotient of 56.7 and 32.2 is {}",quotient);
+
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("the remainder of 43 and 5 is {}",remainder);
+}
---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let special_character = '@';  //default
+    let alphabet:char = 'B';
+    let surname = "Ntekop";
+    let first_name = "Kendara";
+    let middle_name = "Rhoni";
+
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, middle_name,first_name);
+
+}
