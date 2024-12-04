7ccf1e5d6eebdc792702c4fab246ab29c12ba440
kntekopCOS101 <danmusausmain2@gmail.com>
2024-12-04 20:06:19 +0100

    (kendarapractice)

--- week-7/project_1/src/main.rs
+++ week-7/project_1/src/main.rs
@@ -29,12 +29,74 @@ println!("Area of paralellogram = base * altitude");
 println!("Area of cube = 6 * (length)^2");
 println!("Volume of cylinder = pi * rad^2 * height");
 
-println!("If you want to calculate the Area Of Trapeziun, kindly type AOT");
-io::stdin().read_line(&mut input1).expect("Failed to read input");
-println!("If you want to calculate the Area Of Rhombus, kindly type AOR");
-println!("If you want to calculate the Area Of paralellogram, kindly type AOP");
-println!("If you want to calculate the Area Of Cube, kindly type AOC");
-println!("If you want to calculate the Volume Of cylinder, kindly type VOC");
+println!("What will you like to calculate?");
+io::stdin().read_line(&mut input1).expect("Failed to input value");
+
+if input1.trim() == "AOT" {
+    println!("Input the height of the trapezium");
+    io::stdin().read_line(&mut input2).expect("Failed to read input");
+    let h:f64 = input2.trim().parse().expect("Failed to read input");
+    println!("Input base 1 of the trapezium");
+    io::stdin().read_line(&mut input3).expect("Failed to read input");
+    let b1:f64 = input3.trim().parse().expect("Failed to read input");
+    println!("Input base 2 of the trapezium");
+    io::stdin().read_line(&mut input4).expect("Failed to read input");
+    let b2:f64 = input4.trim().parse().expect("Failed to read input");
+
+    let sum:f64 = b1 + b2;
+    let aot:f64 = h / 2.0 * sum;
+    println!("AREA = {}", aot);
+}
+
+else if input1.trim() == "AOR" {
+     println!("Input daigonal 1 of the rhombus");
+    io::stdin().read_line(&mut input5).expect("Failed to read input");
+    let d1:f64 = input5.trim().parse().expect("Failed to read input");
+    println!("Input diagonal 2 of the rhombus");
+    io::stdin().read_line(&mut input6).expect("Failed to read input");
+    let d2:f64 = input6.trim().parse().expect("Failed to read input");
+
+    let aor:f64 = 0.5 * d1 * d2;
+    println!("AREA = {}", aor);
+
+}
+else if input1.trim() == "AOP"  {
+     println!("Input the base of the parallelogram");
+    io::stdin().read_line(&mut input7).expect("Failed to read input");
+    let b:f64 = input7.trim().parse().expect("Failed to read input");
+    println!("Input the altitude of the parallelogram");
+    io::stdin().read_line(&mut input8).expect("Failed to read input");
+    let a:f64 = input8.trim().parse().expect("Failed to read input");
+
+
+    let aop:f64 = b * a;
+    println!("AREA = {}", aop);
+}
+ 
+ else if input1.trim() == "AOC"  {
+     println!("Input the length of the cube");
+    io::stdin().read_line(&mut input9).expect("Failed to read input");
+    let l:f64 = input9.trim().parse().expect("Failed to read input");
+
+
+    let aoc:f64 = 6.0  * l * l;
+    println!("AREA = {}", aoc);
+ }
+
+ else if input1.trim() == "VOC" {
+     println!("Input the value of thr radius");
+    io::stdin().read_line(&mut input10).expect("Failed to read input");
+    let r:f64 = input10.trim().parse().expect("Failed to read input");
+    println!("Input the value of the height");
+    io::stdin().read_line(&mut input11).expect("Failed to read input");
+    let h:f64 = input11.trim().parse().expect("Failed to read input");
+    
+
+    let pi:f64 = 3.142;
+    let voc:f64 = pi * r * r * h;
+    println!("AREA = {}", voc);
+ }
+
 
 
 }
